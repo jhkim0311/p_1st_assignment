aaa4ec4ec92629bcaa203931ffc465a95ddc6661
jhkim0311 <31675698+jhkim0311@users.noreply.github.com>
2017-09-10 13:19:24 +0900

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+	printf("Student ID : 2017301021\n");
+	printf("Name : Kim ji hyun\n");
+	printf("Phone number : 010 5376 8198\n");
+	printf("Github address : jhkim311@naver.com\n");
+return 0;
+}
